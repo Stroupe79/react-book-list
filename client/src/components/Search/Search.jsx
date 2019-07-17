6a6a8f972d9e9c9337aeecf6ab831fe9eed1bb9f
Chris Stroupe <cstroupe79@gmail.com>
2019-07-16 19:32:31 -0500

    working state change on search

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -5,11 +5,37 @@ import API from "../../utils/API";
 
 class Search extends Component {
 
+    state= {
+        query: ""
+    };
+
     searchBooks(props) {
         API.searchBooks(props.searchBox)
         .then(res => API.saveBook(res))
         .catch(err => console.log(err));
-    }
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.title && this.state.author) {
+          API.saveBook({
+            title: this.state.title,
+            author: this.state.author,
+            synopsis: this.state.synopsis
+          })
+            .then(res => this.loadBooks())
+            .catch(err => console.log(err));
+        }
+      };
+    
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+        console.log(this.state)
+      };
+    
 
     // saveBook() {
     //     API.saveBook
@@ -22,7 +48,7 @@ class Search extends Component {
                 <div className="panel-body">
                 Type a book title or author to search!
                 </div>
-                <input type="search"></input>
+                <input onChange={this.handleInputChange} type="search"></input>
                 <button className="btn btn-primary">Search!</button>
             </div>
             </div>
